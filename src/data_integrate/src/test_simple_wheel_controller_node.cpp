865f85d2ce967b335de15ac31ae73d4232b6f29f
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-10 01:39:35 +0900

    refactor: 운전 알고리즘을 SquareDriver 클래스로 분리
    나중에 여러 개의 운전 알고리즘을 만들어놓고 상황에 맞게 교체해서 쓸 수
    있을 지도...?

--- src/data_integrate/src/test_simple_wheel_controller_node.cpp
+++ src/data_integrate/src/test_simple_wheel_controller_node.cpp
@@ -2,8 +2,6 @@
  * @file SimpleWheelController를 테스트하기 위한 노드입니다.
  */
 
-#include <cmath>
-
 #include <angles/angles.h>
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
@@ -11,7 +9,7 @@
 #include "../include/simple_wheel_controller.h"
 
 /**
- * 현재 로봇이 실행하고 있는 동작 상태를 나타냅니다.
+ * 현재 로봇이 실행하고 있는 운행 상태를 나타냅니다.
  */
 enum class MoveActionState
 {
@@ -19,6 +17,112 @@ enum class MoveActionState
   Line,  ///< 정사각형의 변을 따라 직진한다.
   Turn,  ///< 정사각형의 꼭짓점에서 회전한다.
 };
+
+/**
+ * 반시계 방향으로 정사각형을 그리는 운전 알고리즘. 정사각형을 모두 그리면 잠시 쉬었다가 다시 시작합니다.
+ */
+class SquareDriver
+{
+public:
+  /**
+   * @param side_length   한 변의 길이 (meters)
+   * @param line_speed    한 변을 따라 이동할 때의 선속도 (m/s)
+   * @param turn_speed    꼭짓점에서 회전할 때의 각속도 (rad/s)
+   * @param rest_duration 정사각형을 1번 그린 후 쉬는 시간 (seconds)
+   */
+  SquareDriver(double side_length, double line_speed, double turn_speed, double rest_duration);
+
+  /**
+   * 현재 운행 상태를 시간이 @p time_passed 만큼 지나간 후의 상태로 변경합니다.
+   * @param time_passed 지나간 시간
+   */
+  void updateState(double time_passed);
+
+  /**
+   * 현재 운행 상태에 알맞게 바퀴 컨트롤러를 업데이트합니다.
+   */
+  void updateController(SimpleWheelController& wheel_controller) const;
+
+private:
+  double side_length_ = 1;
+  double line_speed_ = 1;
+  double turn_speed_ = 1;
+  double rest_duration_ = 1;
+  double action_timer_ = 0;
+  double line_segment_counter_ = 0;
+  MoveActionState action_state_ = MoveActionState::Stop;
+};
+
+SquareDriver::SquareDriver(double side_length, double line_speed, double turn_speed, double rest_duration)
+  : side_length_(side_length), line_speed_(line_speed), turn_speed_(turn_speed), rest_duration_(rest_duration)
+{
+  // 비워놓음
+}
+
+void SquareDriver::updateState(double time_passed)
+{
+  action_timer_ -= time_passed;
+  if (action_timer_ > 0)
+    return;
+  // 타이머가 0이 되면 다음 운행 상태로 전환합니다.
+
+  ROS_ASSERT(line_speed_ > 0);
+  ROS_ASSERT(turn_speed_ > 0);
+  ROS_ASSERT(rest_duration_ > 0);
+
+  // 직진 -> 반시계 회전 -> 직진 -> 반시계 회전 -> ... 을 반복하며 정사각형을 그립니다.
+  // 처음 위치에 돌아오면 잠시 정지합니다.
+  switch (action_state_)
+  {
+    case MoveActionState::Stop:
+      action_state_ = MoveActionState::Line;
+      action_timer_ = side_length_ / line_speed_;
+      ROS_INFO("Moving straight for %f second(s)", action_timer_);
+      break;
+    case MoveActionState::Line:
+      action_state_ = MoveActionState::Turn;
+      action_timer_ = angles::from_degrees(90) / turn_speed_;
+      ROS_INFO("Turning left for %f second(s)", action_timer_);
+      break;
+    case MoveActionState::Turn:
+      // 4번째 변까지 그렸다면 잠시 정지
+      if (line_segment_counter_ >= 3)
+      {
+        action_state_ = MoveActionState::Stop;
+        action_timer_ = rest_duration_;
+        line_segment_counter_ = 0;
+        ROS_INFO("Stopping for %f second(s)", rest_duration_);
+      }
+      else
+      {
+        action_state_ = MoveActionState::Line;
+        action_timer_ = side_length_ / line_speed_;
+        ++line_segment_counter_;
+        ROS_INFO("Moving straight for %f second(s)", action_timer_);
+      }
+      break;
+    default:
+      ROS_ASSERT_MSG(0, "Invalid action state: %d", action_state_);
+  }
+}
+
+void SquareDriver::updateController(SimpleWheelController& wheel_controller) const
+{
+  switch (action_state_)
+  {
+    case MoveActionState::Stop:
+      wheel_controller.stop();
+      break;
+    case MoveActionState::Line:
+      wheel_controller.moveLinear(line_speed_);
+      break;
+    case MoveActionState::Turn:
+      wheel_controller.turn(turn_speed_);
+      break;
+    default:
+      ROS_ASSERT_MSG(0, "Invalid action state: %d", action_state_);
+  }
+}
 
 int main(int argc, char** argv)
 {
@@ -30,86 +134,23 @@ int main(int argc, char** argv)
 
   SimpleWheelController wheel_controller(left_wheel, right_wheel);
 
-  int action_counter = 0;
-  int line_segment_counter = 0;
-  auto action_state = MoveActionState::Stop;
+  ros::Duration publish_interval(0.025);  // seconds
 
-  const double PUBLISH_INTERVAL = 0.025;
-  ros::Duration publish_interval(PUBLISH_INTERVAL);
-
-  // 운전 관련 상수
-  const double LINE_SPEED = 0.5;   // m/s
-  const double LINE_LENGTH = 1.0;  // meters
-  const double TURN_SPEED = angles::from_degrees(30);
-  const double TURN_ANGLE = angles::from_degrees(90);
-  const double STOP_DURATION = 1.5;  // seconds
+  const double SIDE_LENGTH = 1.0;                      // meters
+  const double LINE_SPEED = 0.5;                       // m/s
+  const double TURN_SPEED = angles::from_degrees(90);  // rad/s
+  const double REST_DURATION = 1.5;                    // seconds
+  SquareDriver driver(SIDE_LENGTH, LINE_SPEED, TURN_SPEED, REST_DURATION);
 
   ROS_INFO("SimpleWheelController test node is publishing on:\n  %s\n  %s", left_wheel.getTopic().c_str(),
            right_wheel.getTopic().c_str());
 
   while (ros::ok())
   {
-    // 테스트용 코드
-    // 로봇이 직진 -> 반시계 회전 -> 직진 -> 반시계 회전 -> ... 을 반복하며 가로, 세로 1m의 정사각형을 그리게 합니다.
-    // 처음 위치에 돌아오면 1초간 정지합니다.
-
-    // 현재 상태를 유지하는 카운터 감소
-    --action_counter;
-
-    // 카운터가 0이 되면 다음 상태로 전환
-    if (action_counter <= 0)
-    {
-      switch (action_state)
-      {
-        case MoveActionState::Stop:
-          action_state = MoveActionState::Line;
-          action_counter = std::round(LINE_LENGTH / LINE_SPEED / PUBLISH_INTERVAL);
-          ROS_INFO("Moving straight for %f second(s)", LINE_LENGTH / LINE_SPEED);
-          break;
-        case MoveActionState::Line:
-          action_state = MoveActionState::Turn;
-          action_counter = std::round(TURN_ANGLE / TURN_SPEED / PUBLISH_INTERVAL);
-          ROS_INFO("Turning left for %f second(s)", TURN_ANGLE / TURN_SPEED);
-          break;
-        case MoveActionState::Turn:
-          // 4번째 변까지 그렸다면 잠시 정지
-          if (line_segment_counter >= 3)
-          {
-            action_state = MoveActionState::Stop;
-            action_counter = std::round(STOP_DURATION / PUBLISH_INTERVAL);
-            line_segment_counter = 0;
-            ROS_INFO("Stopping for %f second(s)", STOP_DURATION);
-          }
-          else
-          {
-            action_state = MoveActionState::Line;
-            action_counter = std::round(LINE_LENGTH / LINE_SPEED / PUBLISH_INTERVAL);
-            ++line_segment_counter;
-            ROS_INFO("Moving straight for %f second(s)", LINE_LENGTH / LINE_SPEED);
-          }
-          break;
-        default:
-          ROS_ASSERT_MSG(0, "Invalid action state: %d", action_state);
-      }
-    }
-
-    // 현재 상태에 맞게 바퀴 속도를 설정
-    switch (action_state)
-    {
-      case MoveActionState::Stop:
-        wheel_controller.stop();
-        break;
-      case MoveActionState::Line:
-        wheel_controller.moveLinear(LINE_SPEED);
-        break;
-      case MoveActionState::Turn:
-        wheel_controller.turn(TURN_SPEED);
-        break;
-      default:
-        ROS_ASSERT_MSG(0, "Invalid action state: %d", action_state);
-    }
-
+    driver.updateState(publish_interval.toSec());
+    driver.updateController(wheel_controller);
     wheel_controller.publish();
+
     publish_interval.sleep();
   }
 
