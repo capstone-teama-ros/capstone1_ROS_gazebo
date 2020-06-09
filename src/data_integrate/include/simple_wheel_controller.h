9c63596be20136788d1c047115393b458e54db31
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-10 01:09:18 +0900

    feat: SimpleWheelController 코드 추가

---
+++ src/data_integrate/include/simple_wheel_controller.h
@@ -0,0 +1,55 @@
+#ifndef DATA_INTEGRATE_SIMPLE_WHEEL_CONTROLLER_H
+#define DATA_INTEGRATE_SIMPLE_WHEEL_CONTROLLER_H
+
+#include <ros/ros.h>
+
+/**
+ * 아주 기초적인 동작만을 수행할 수 있는 운전 컨트롤러입니다.
+ * 등속도 선운동, 등각속도 제자리 회전 운동, 정지의 3가지 동작만 수행 가능합니다.
+ */
+class SimpleWheelController
+{
+public:
+  /**
+   * @param left_wheel 왼쪽 바퀴에 메시지를 전달할 publisher
+   * @param right_wheel 오른쪽 바퀴에 메시지를 전달할 publisher
+   */
+  SimpleWheelController(const ros::Publisher &left_wheel, const ros::Publisher &right_wheel);
+
+  /**
+   * 일정한 속도로 선운동을 합니다. 회전은 하지 않습니다.
+   * @p linear_speed 가 0보다 작으면 후진합니다.
+   * 회전 중이었다면 회전을 중지합니다.
+   *
+   * @param linear_speed 선속도 (단위: m/s)
+   */
+  void moveLinear(double linear_speed);
+
+  /**
+   * 제자리에서 일정한 각속도로 회전합니다. 등속도 운동 중이었다면 등속도 운동을 중지합니다.
+   * 양수값을 주면 좌회전(반시계 방향), 음수값을 주면 우회전(시계 방향)합니다.
+   * 선운동 중이였다면 선운동을 중지합니다.
+   *
+   * @param angular_speed 각속도 (단위: rad/s)
+   */
+  void turn(double angular_speed);
+
+  /**
+   * 선운동 또는 회전을 중지하고 제자리에 정지합니다.
+   * 이미 정지한 경우 아무런 동작을 하지 않습니다.
+   */
+  void stop();
+
+  /**
+   * 각 바퀴의 publisher에게 현재 진행하고 있는 동작에 알맞는 메시지를 전달합니다.
+   */
+  void publish() const;
+
+private:
+  double linear_speed_ = 0;
+  double angular_speed_ = 0;
+  const ros::Publisher &left_wheel_;
+  const ros::Publisher &right_wheel_;
+};
+
+#endif  // DATA_INTEGRATE_SIMPLE_WHEEL_CONTROLLER_H
