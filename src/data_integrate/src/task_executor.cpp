6aefaef906eabaceca0ad7cc9d356c5c268f1f97
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-12 22:53:30 +0900

    fix: TaskExecutor 코드 오류 수정
    5eaa9f0dfaa5a1adce77524f80c0658329f7edbb 에서 TaskExecutor를 구현할 때
    제대로 확인하지 못하고 넘어갔던 오류를 수정합니다. task_executor.cpp를
    컴파일하지 않아서 그 동안 확인하지 못했습니다.

--- src/data_integrate/src/task_executor.cpp
+++ src/data_integrate/src/task_executor.cpp
@@ -1,8 +1,10 @@
 #include "data_integrate/task_executor.h"
 
 #include <ros/ros.h>
+#include <utility>
 
-void TaskExecutor::runTaskInLoop(double time_passed_after_last, double time_until_next)
+void TaskExecutor::runTaskInLoop(double time_passed_after_last, double time_until_next,
+                                 SimpleWheelController &wheel_controller)
 {
   ROS_ASSERT_MSG(task_, "Missing task");
 
@@ -10,16 +12,16 @@ void TaskExecutor::runTaskInLoop(double time_passed_after_last, double time_unti
   auto nextTask = task_->updateTaskOrMakeNextTask(time_passed_after_last);
   if (nextTask)
   {
-    task_ = nextTask;
+    task_ = std::move(nextTask);
   }
 
   // 업데이트한 작업의 결정에 따라 바퀴 컨트롤러의 상태를 업데이트합니다.
-  task_->updateWheelController(wheel_controller);
+  task_->updateWheelController(time_until_next, wheel_controller);
   // 바퀴 컨트롤러를 통해 메시지를 전파합니다.
   wheel_controller.publish();
 }
 
 void TaskExecutor::overrideTask(Task::TaskPtr task)
 {
-  task_ = task;
+  task_ = std::move(task);
 }
