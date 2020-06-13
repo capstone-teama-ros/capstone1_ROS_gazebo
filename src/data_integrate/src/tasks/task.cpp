1ce7e060596de294ed571bcadef9da7c2e2e838f
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-14 07:04:36 +0900

    feat: 디버그 메시지에 각 작업의 tick() 결과 출력

--- src/data_integrate/src/tasks/task.cpp
+++ src/data_integrate/src/tasks/task.cpp
@@ -3,6 +3,21 @@
 unsigned int Task::tick_depth_ = 0;
 unsigned int Task::halt_depth_ = 0;
 
+const char *toString(TaskResult result)
+{
+  switch (result)
+  {
+    case TaskResult::Success:
+      return "success";
+    case TaskResult::Failure:
+      return "failure";
+    case TaskResult::Running:
+      return "running";
+    default:
+      ROS_INVALID_TASK_RESULT(result);
+  }
+}
+
 TaskResult Task::tick(Blackboard &blackboard)
 {
   ROS_DEBUG("%*s%s::tick()", tick_depth_ * 2, "", name());
@@ -11,6 +26,7 @@ TaskResult Task::tick(Blackboard &blackboard)
   auto result = doTick(blackboard);
   --tick_depth_;
 
+  ROS_DEBUG("%*s\\- %s [%s]", tick_depth_ * 2, "", toString(result), name());
   return result;
 }
 
