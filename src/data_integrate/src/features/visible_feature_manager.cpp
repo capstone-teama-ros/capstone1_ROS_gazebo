5a8026a349d271eb74a7210ef176ab02aef6a3ec
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-16 08:44:02 +0900

    feat: 기둥 탐지 로직을 VisibleFeatureManager에 추가

--- src/data_integrate/src/features/visible_feature_manager.cpp
+++ src/data_integrate/src/features/visible_feature_manager.cpp
@@ -2,6 +2,173 @@
 
 #include <ros/ros.h>
 #include <cmath>
+#include <limits>
+#include <vector>
+
+using RelPointList = std::vector<RelPoint>;
+using RelPointClusterList = std::vector<RelPointList>;
+
+/**
+ * 도우미 구조체: 직사각형을 나타냅니다.
+ */
+struct Rect
+{
+  double x;
+  double y;
+  double width;
+  double height;
+};
+
+/**
+ * 도우미 함수: @p cluster 에 속한 점들 중에서 @p point 와 가장 가까운 점과 @p point 사이의 거리를 구합니다.
+ * @p cluster 가 비어있으면 무한대를 리턴합니다.
+ */
+double computeDistanceToNearestPointInCluster(const RelPoint& point, const RelPointList& cluster)
+{
+  auto nearest_distance_sq = std::numeric_limits<double>::infinity();
+
+  for (auto& cluster_point : cluster)
+  {
+    auto distance_sq = std::norm(
+        std::complex<double>(cluster_point.getRelX() - point.getRelX(), cluster_point.getRelY() - point.getRelY()));
+    if (distance_sq < nearest_distance_sq)
+    {
+      nearest_distance_sq = distance_sq;
+    }
+  }
+
+  // 못 찾았을 경우
+  if (std::isinf(nearest_distance_sq))
+  {
+    return nearest_distance_sq;
+  }
+
+  return std::sqrt(nearest_distance_sq);
+}
+
+/**
+ * 도우미 함수: @p points 에 속한 점들의 좌표의 평균점을 구합니다.
+ */
+RelPoint computeAveragePoint(const RelPointList& points)
+{
+  double x_sum = 0, y_sum = 0;
+  for (auto& point : points)
+  {
+    x_sum += point.getRelX();
+    y_sum += point.getRelY();
+  }
+  return RelPoint::fromRelXY(x_sum / points.size(), y_sum / points.size());
+}
+
+/**
+ * 도우미 함수: 여러 개의 점들을 클러스터로 묶습니다. 이때 각 클러스터의 점들 중 가장 가까운 점까지의 거리를 사용합니다.
+ * 주의: 비효율적입니다. O(n^2)
+ *
+ * @param points      점들의 목록. 무한히 먼 거리에 있는 점들은 제외합니다.
+ * @param threshold   같은 클러스터에 속한 점으로 취급하기 위한 점 간의 최대 거리
+ * @returns 검출한 기둥들의 좌표
+ */
+RelPointClusterList clusterByNearestPointDistance(const RelPointList& points, double threshold)
+{
+  RelPointClusterList clusters;
+
+  for (auto& point : points)
+  {
+    RelPointList* nearest_cluster = nullptr;
+    auto nearest_distance = std::numeric_limits<double>::infinity();
+
+    // 가장 가까운 클러스터를 찾는다
+    for (auto& cluster : clusters)
+    {
+      auto distance = computeDistanceToNearestPointInCluster(point, cluster);
+      if (distance <= threshold)
+      {
+        nearest_cluster = &cluster;
+        nearest_distance = distance;
+      }
+    }
+
+    if (nearest_cluster)
+    {
+      // 가장 가까운 클러스터에 점 추가
+      nearest_cluster->emplace_back(point);
+    }
+    else
+    {
+      // 새로운 클러스터를 만들어 넣는다.
+      clusters.emplace_back(1, point);
+    }
+  }
+
+  return clusters;
+}
+
+/**
+ * 도우미 함수: 클러스터에 속한 모든 점을 포함하는 가장 작은 직사각형을 구합니다.
+ * @p cluster 가 비어있으면 { 0, 0, 0, 0 }을 돌려줍니다.
+ *
+ * @param points  클러스터에 속한 점들의 목록
+ * @returns 클러스터를 감싸는 직사각형의 위치 및 크기 정보
+ */
+Rect findBoundingBox(const RelPointList& points)
+{
+  if (points.size() == 0)
+    return {};
+
+  double x_min = std::numeric_limits<double>::infinity(), x_max = -std::numeric_limits<double>::infinity();
+  double y_min = std::numeric_limits<double>::infinity(), y_max = -std::numeric_limits<double>::infinity();
+  for (auto& point : points)
+  {
+    auto x = point.getRelX(), y = point.getRelY();
+    if (x < x_min)
+      x_min = x;
+    if (x > x_max)
+      x_max = x;
+    if (y < y_min)
+      y_min = y;
+    if (y > y_max)
+      y_max = y;
+  }
+
+  ROS_ASSERT(x_min <= x_max);
+  ROS_ASSERT(y_min <= y_max);
+  return { x_min, y_min, x_max - x_min, y_max - y_min };
+}
+
+/**
+ * 도우미 함수: LIDAR 데이터로부터 기둥을 검출합니다.
+ *
+ * @param points      LIDAR로 검출한 점들의 목록. 무한히 먼 거리에 있는 점들은 제외합니다.
+ * @param threshold   같은 기둥 내 점으로 취급하기 위한 점 간의 최대 거리 (meters)
+ * @param min_points  한 기둥 내에 포함된 점의 최소 갯수. 이보다 점이 적으면 노이즈로 취급해 무시합니다.
+ * @param max_points  한 기둥 내에 포함된 점의 최대 갯수. 이보다 점이 많으면 기둥이 아닌 벽으로 판단해 무시합니다.
+ * @param max_cluster_size  클러스터의 최대 크기 (meters).
+ * 클러스터의 bounding box의 너비나 높이가 이보다 크면 기둥이 아닌 벽으로 판단해 무시합니다.
+ * @returns 검출한 기둥들의 좌표
+ */
+RelPointList findColumns(const RelPointList& points, double threshold, size_t min_points, size_t max_points,
+                         double max_cluster_size)
+{
+  ROS_ASSERT(threshold >= 0);
+  ROS_ASSERT(min_points <= max_points);
+  ROS_ASSERT(max_cluster_size >= 0);
+
+  auto clusters = clusterByNearestPointDistance(points, threshold);
+
+  RelPointList columns;
+  for (auto& cluster : clusters)
+  {
+    auto bounding_box = findBoundingBox(cluster);
+
+    if (min_points <= cluster.size() && cluster.size() <= max_points && bounding_box.width <= max_cluster_size &&
+        bounding_box.height <= max_cluster_size)
+    {
+      columns.emplace_back(computeAveragePoint(cluster));
+    }
+  }
+
+  return columns;
+}
 
 void VisibleFeatureManager::subscribeToLidar(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
@@ -24,6 +191,19 @@ void VisibleFeatureManager::subscribeToLidar(const sensor_msgs::LaserScan::Const
 
     ++i;
   }
+
+  // 기둥을 검출합니다.
+  const double COLUMN_THRESHOLD = 0.3;  // meters
+  const size_t COLUMN_MIN_POINTS = 2;
+  const size_t COLUMN_MAX_POINTS = 20;  // 너무 작게 하면 가까운 기둥을 인식하지 못하므로, 적당히 큰 값으로 합니다.
+  const double COLUMN_MAX_CLUSTER_SIZE = 0.2;  // meters
+
+  columns_ =
+      findColumns(lidar_points_, COLUMN_THRESHOLD, COLUMN_MIN_POINTS, COLUMN_MAX_POINTS, COLUMN_MAX_CLUSTER_SIZE);
+
+  const size_t EXPECTED_COLUMN_COUNT = 4;
+  ROS_WARN_COND(columns_.size() > EXPECTED_COLUMN_COUNT, "%lu columns detected (expected %lu)", columns_.size(),
+                EXPECTED_COLUMN_COUNT);
 }
 
 void VisibleFeatureManager::subscribeToCamera(const core_msgs::ball_position::ConstPtr& msg)
