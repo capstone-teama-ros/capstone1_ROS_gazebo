cb2f8aed3222f1e560a8f96c3f434dc4e3c73c45
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-17 00:05:35 +0900

    test: 직선 속도성분-회전 속도성분을 offset에 대한 일차함수로 계산

--- src/data_integrate/src/tasks/line_tracer.cpp
+++ src/data_integrate/src/tasks/line_tracer.cpp
@@ -4,7 +4,6 @@
 
 void LineTracer::doHalt(Blackboard &blackboard)
 {
-  line_position_ = LinePosition::None;
   blackboard.useSimpleWheelController = true;
 }
 
@@ -20,55 +19,22 @@ TaskResult LineTracer::doTick(Blackboard &blackboard)
   // midpoint의 상대적 위치를 -1 ~ 1 사이의 값으로 변환한다.
   double midpoint_relative = lt_box_midpoint / lt_image_width * 2 - 1;
 
-  if (midpoint_relative < -0.2)
-  {
-    line_position_ = LinePosition::Left;
-  }
-  else if (midpoint_relative <= 0.2)
-  {
-    line_position_ = LinePosition::Center;
-  }
-  else
-  {
-    line_position_ = LinePosition::Right;
-  }
-
   // 속도 파라미터
-  double linear_speed;
-  double linear_speed_slow;
-  double angular_speed_bonus;
-  ros::param::param<double>("~linear_speed", linear_speed, 0);
-  ros::param::param<double>("~linear_speed_slow", linear_speed_slow, 0);
-  ros::param::param<double>("~angular_speed_bonus", angular_speed_bonus, 0);
-  linear_speed /= 0.063;
-  linear_speed_slow /= 0.063;
-  angular_speed_bonus /= 0.063;
+  double speed_base;
+  double speed_turn_factor;
+  ros::param::param<double>("~speed_base", speed_base, 0);
+  ROS_INFO_ONCE("Reading parameter from: %s", ros::names::resolve("~speed_base").c_str());
+  ros::param::param<double>("~speed_turn_factor", speed_turn_factor, 0);
+  ROS_INFO_ONCE("Reading parameter from: %s", ros::names::resolve("~speed_turn_factor").c_str());
+  speed_base /= 0.063;
+  speed_turn_factor /= 0.063;
 
-  switch (line_position_)
-  {
-    case LinePosition::Center:
-      direct_controller.setWheelSpeeds(linear_speed, linear_speed);
-      ROS_INFO("Center (midpoint_relative = %.3f)", midpoint_relative);
-      break;
-    case LinePosition::Left:
-      direct_controller.setWheelSpeeds(linear_speed_slow - angular_speed_bonus,
-                                       linear_speed_slow + angular_speed_bonus);
-      ROS_INFO("Left (midpoint_relative = %.3f)", midpoint_relative);
-      break;
-    case LinePosition::Right:
-      direct_controller.setWheelSpeeds(linear_speed_slow + angular_speed_bonus,
-                                       linear_speed_slow - angular_speed_bonus);
-      ROS_INFO("Right (midpoint_relative = %.3f)", midpoint_relative);
-      break;
-    case LinePosition::None:
-      // 로봇이 검은 줄을 놓쳤다.
-      ROS_WARN("No line found!");
-      direct_controller.setWheelSpeeds(linear_speed, linear_speed);
-      break;
-    default:
-      ROS_ASSERT_MSG(0, "Unexpected line position: %u", static_cast<unsigned int>(line_position_));
-      break;
-  }
+  auto left_speed = speed_base + speed_turn_factor * midpoint_relative;
+  auto right_speed = speed_base - speed_turn_factor * midpoint_relative;
+
+  direct_controller.setWheelSpeeds(left_speed, right_speed);
+  ROS_INFO("midpoint_relative = %.3f, left_speed = %.3f, right_speed = %.3f", midpoint_relative, left_speed,
+           right_speed);
 
   // TODO: 일단은 Line Tracer 모드의 종료 조건을 안 정했다. 나중에 정하자.
   return TaskResult::Running;
